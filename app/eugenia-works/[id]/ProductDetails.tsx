dbf0fb4d9c8e51d2ace7ac16acddcf399bedf934
Yuriy Bespechanskiy <eckhart1967@gmail.com>
2025-06-03 15:14:36 +0300

    11-th commit

--- app/eugenia-works/[id]/ProductDetails.tsx
+++ app/eugenia-works/[id]/ProductDetails.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import styles from '../../styles/product.module.css';
 import type { Language } from '@/lib/types';
 
-// interface ProductDetailsProps {
 interface Props {
   productId: number;
   lang: Language;
